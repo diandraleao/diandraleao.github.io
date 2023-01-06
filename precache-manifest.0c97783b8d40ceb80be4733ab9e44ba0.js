387610de48dc1d629e7b03a64b08b4c85aea956e
Diandra <leao.diandra@gmail.com>
2023-01-06 19:06:31 -0300

    build

---
+++ precache-manifest.0c97783b8d40ceb80be4733ab9e44ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d4b370b9f4cc314a116",
+    "url": "/static/css/main.9a41da52.chunk.css"
+  },
+  {
+    "revision": "4d4b370b9f4cc314a116",
+    "url": "/static/js/main.4d4b370b.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4309b1dd1d46ad2cff847ae46c7f56f0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1169807d38454fd49ac33668cfff3ffa.js"
+  "/precache-manifest.0c97783b8d40ceb80be4733ab9e44ba0.js"
 );
 
 workbox.clientsClaim();
